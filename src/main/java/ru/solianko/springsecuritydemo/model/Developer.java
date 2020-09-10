ba2975e11c04f39742d30f97d81377a0be3ac3c0
Roman Solianko <RSolianko@yandex.ru>
2020-09-10 16:09:04 +0300

    Init commit

---
+++ src/main/java/ru/solianko/springsecuritydemo/model/Developer.java
@@ -0,0 +1,13 @@
+package ru.solianko.springsecuritydemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
---
+++ src/test/java/ru/solianko/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.solianko.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
