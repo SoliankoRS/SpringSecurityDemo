c1784f6c23e1acb747e9a1ecdb616272914adfbb
Roman Solianko <RSolianko@yandex.ru>
2020-09-13 15:52:11 +0300

    Authorization with @PreAuthorize annotation

--- src/main/java/ru/solianko/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/ru/solianko/springsecuritydemo/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package ru.solianko.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,11 +11,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import ru.solianko.springsecuritydemo.model.Permission;
 import ru.solianko.springsecuritydemo.model.Role;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -24,9 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
-                .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
-                .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -37,17 +34,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected UserDetailsService userDetailsService() {
         return new InMemoryUserDetailsManager(
-                User.builder()
-                        .username("admin")
+                User.builder().username("admin")
                         .password(passwordEncoder().encode("admin"))
                         .authorities(Role.ADMIN.getAuthorities())
                         .build(),
-                User.builder()
-                        .username("user")
+                User.builder().username("user")
                         .password(passwordEncoder().encode("user"))
                         .authorities(Role.USER.getAuthorities())
                         .build()
-
         );
     }
 
